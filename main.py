1307adfe04ecfe19fda06ffd49bd147937bf4cdd
szuter <piotrstrzelec1989@gmail.com>
2024-12-03 14:46:43 +0100

    added rotation to player

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)    
             
         screen.fill("black")
         player.draw(screen)
