bafc9759503d791b776b4669321fb35f71285eb2
szuter <piotrstrzelec1989@gmail.com>
2024-12-03 14:03:36 +0100

    create .gitignore and added function to display window

--- main.py
+++ main.py
@@ -2,14 +2,18 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
